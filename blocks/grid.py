dd5a77f014edfa43ddf6ef7755a12ebe6a342290
mh <martin.hafemann@htwk-leipzig.de>
2023-11-28 16:36:23 +0100

    Removed binary variables and corresponding constraints.

--- blocks/grid.py
+++ blocks/grid.py
@@ -19,36 +19,27 @@ class Grid:
         block.overall_power = Var(t, domain=Reals)
         block.supply_power = Var(t, domain=NonNegativeReals)
         block.feedin_power = Var(t, domain=NonNegativeReals)
-        block.supply_bin = Var(t, within=Binary)
-        block.feedin_bin = Var(t, within=Binary)
 
         block.power_in = Port()
         block.power_in.add(block.feedin_power, 'power', Port.Extensive, include_splitfrac=False)
-        # initialize={'power': (block.feedin_power, Port.Extensive)})
         block.power_out = Port()
         block.power_out.add(block.supply_power, 'power', Port.Extensive, include_splitfrac=False)
-        # initialize={'power': (block.supply_power, Port.Extensive)})
 
 
         # Define construction rules for constraints
         def max_supply_power_rule(_block, i):
             """Rule for the maximal supply power."""
-            return _block.supply_power[i] <= self.data.loc['Max', 'Power'] * _block.supply_bin[i]
+            return _block.supply_power[i] <= self.data.loc['Max', 'Power']
         
 
         def max_feedin_power_rule(_block, i):
             """Rule for the maximal feed in power."""
-            return _block.feedin_power[i] <= self.data.loc['Max', 'Power'] * _block.feedin_bin[i]
+            return _block.feedin_power[i] <= self.data.loc['Max', 'Power']
         
 
         def overall_power_rule(_block, i):
             """Rule for calculating the overall power."""
             return _block.overall_power[i] == _block.supply_power[i] - _block.feedin_power[i]
-
-
-        def binary_rule(_block, i):
-            """Rule for restricting simultaneous supply and feed in."""
-            return _block.supply_bin[i] + _block.feedin_bin[i] <= 1
         
 
         # Define constraints
@@ -64,7 +55,4 @@ class Grid:
             t,
             rule=overall_power_rule
         )
-        block.binary_constraint = Constraint(
-            t,
-            rule=binary_rule
-        )
+        
