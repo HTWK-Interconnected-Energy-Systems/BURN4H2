c2c492eaed76ab761100bf7d04d188a3b78a5131
mh <martin.hafemann@htwk-leipzig.de>
2023-11-28 13:13:16 +0100

    Added files for pv asset construction and paramatrisation.

---
+++ blocks/res.py
@@ -0,0 +1,38 @@
+from pyomo.environ import *
+from pyomo.network import *
+
+class Photovoltaics:
+    """Class for constructing photovoltaics asset objects"""
+
+    def __init__(self, data, capacity_factors) -> None:
+        self.data = data
+        self.capacity_factors = capacity_factors
+    
+
+    def pv_block_rule(self, block):
+        """Rule for creating a photovoltaics block with default components and constraints."""
+        # Get index from model
+        t = block.model().t
+
+
+        # Define components
+        block.power = Var(t, domain=NonNegativeReals)
+
+        block.power_out = Port()
+        block.power_out.add(block.power, 'power', Port.Extensive, include_splitfrac=False)
+
+
+        # Define construction rules for constraints
+        def power_generation_rule(_block, i):
+            """Rule for calculating the power generation of the photovoltaics."""
+            installed_power = self.data.loc['value', 'installed_power']
+            inverter_efficiency = self.data.loc['value', 'inverter_efficiency']
+            capacity_factors = self.capacity_factors['capacity_factor']
+            return _block.power[i] == installed_power * inverter_efficiency * capacity_factors[i]
+        
+
+        # Define constraints
+        block.power_generation_constraint = Constraint(
+            t,
+            rule=power_generation_rule
+        )
