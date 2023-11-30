2d8fb5cbf1490dcf09d358ee1b9fdeae2b9f2b24
mh <martin.hafemann@htwk-leipzig.de>
2023-11-30 15:25:20 +0100

    Fixed references in electrolyzer.py

--- blocks/electrolyzer.py
+++ blocks/electrolyzer.py
@@ -16,8 +16,8 @@ class Electrolyzer:
         # Define components
         block.bin = Var(t, within=Binary)
         block.hydrogen = Var(t, domain=NonNegativeReals)
-        block.power = Var(t, domain=NonNegativeReals)
-        block.heat = Var(t, domain=NonNegativeReals)
+        block.power = Var(t, domain=NonPositiveReals)
+        block.water = Var(t, domain=NonNegativeReals)
 
         block.power_in = Port()
         block.power_in.add(block.power, 'power', block.power_in.Extensive, include_splitfrac=False)
@@ -25,20 +25,20 @@ class Electrolyzer:
 
         def power_max_rule(_block, i):
             """Rule for the maximal power consumption."""
-            return _block.power[i] <= self.data.loc['max', 'power'] * _block.bin[i]
+            return _block.power[i] >= -self.data.loc['max', 'power'] * _block.bin[i]
         
 
         def power_min_rule(_block, i):
             """Rule for the minimal power consumption."""
-            return self.data.loc['min', 'power'] * _block.bin[i] <= _block.power[i]
+            return -self.data.loc['min', 'power'] * _block.bin[i] >= _block.power[i]
         
 
         def hydrogen_depends_on_power_rule(_block, i):
             """Rule for the dependencies between hydrogen output and power demand."""
             hydrogen_min = self.data.loc['min', 'hydrogen']
             hydrogen_max = self.data.loc['max', 'hydrogen']
-            power_min = self.data.loc['min', 'power']
-            power_max = self.data.los['max', 'power']
+            power_min = -self.data.loc['min', 'power']
+            power_max = -self.data.loc['max', 'power']
 
             a = (hydrogen_max - hydrogen_min) / (power_max - power_min)
             b = hydrogen_max - a * power_max
@@ -51,7 +51,7 @@ class Electrolyzer:
             hydrogen_min = self.data.loc['min', 'hydrogen']
             hydrogen_max = self.data.loc['max', 'hydrogen']
             water_min = self.data.loc['min', 'water']
-            water_max = self.data.los['max', 'water']
+            water_max = self.data.loc['max', 'water']
 
             a = (water_max - water_min) / (hydrogen_max - hydrogen_min)
             b = water_max - a * hydrogen_max
