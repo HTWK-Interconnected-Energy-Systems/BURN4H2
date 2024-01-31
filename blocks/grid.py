496638a326d1354f7e1af968d71e62d91da4cea4
mh <martin.hafemann@htwk-leipzig.de>
2024-01-31 15:14:18 +0100

    Updated method names.

--- blocks/grid.py
+++ blocks/grid.py
@@ -16,50 +16,50 @@ class Grid:
         t = block.model().t
 
         # Declare components
-        block.overall_power = Var(t, domain=Reals)
-        block.supply_power = Var(t, domain=NonNegativeReals)
-        block.feedin_power = Var(t, domain=NonNegativeReals)
+        block.power_balance = Var(t, domain=Reals)
+        block.power_supply = Var(t, domain=NonNegativeReals)
+        block.power_feedin = Var(t, domain=NonNegativeReals)
 
         block.power_in = Port()
         block.power_in.add(
-            block.feedin_power,
+            block.power_feedin,
             'power',
             Port.Extensive,
             include_splitfrac=False
             )
         block.power_out = Port()
         block.power_out.add(
-            block.supply_power,
+            block.power_supply,
             'power',
             Port.Extensive,
             include_splitfrac=False
             )
 
         # Declare construction rules for constraints
-        def max_supply_power_rule(_block, i):
+        def max_power_supply_rule(_block, i):
             """Rule for the maximal supply power."""
-            return _block.supply_power[i] <= self.data.loc['max', 'power']
+            return _block.power_supply[i] <= self.data.loc['max', 'power']
         
-        def max_feedin_power_rule(_block, i):
+        def max_power_feedin_rule(_block, i):
             """Rule for the maximal feed in power."""
-            return _block.feedin_power[i] <= self.data.loc['max', 'power']
+            return _block.power_feedin[i] <= self.data.loc['max', 'power']
         
-        def overall_power_rule(_block, i):
+        def power_balance_rule(_block, i):
             """Rule for calculating the overall power."""
-            return _block.overall_power[i] == _block.supply_power[i] - _block.feedin_power[i]
+            return _block.power_balance[i] == _block.power_supply[i] - _block.power_feedin[i]
         
         # Define constraints
-        block.max_supply_power_constraint = Constraint(
+        block.max_power_supply_constraint = Constraint(
             t,
-            rule=max_supply_power_rule
+            rule=max_power_supply_rule
         )
-        block.max_feedin_power_constraint = Constraint(
+        block.max_power_feedin_constraint = Constraint(
             t,
-            rule=max_feedin_power_rule
+            rule=max_power_feedin_rule
         )
-        block.overall_power_constraint = Constraint(
+        block.power_balance_constraint = Constraint(
             t,
-            rule=overall_power_rule
+            rule=power_balance_rule
         )
     
 
@@ -71,50 +71,50 @@ class Grid:
         t = block.model().t
 
         # Declare components
-        block.overall_hydrogen = Var(t, domain=Reals)
-        block.supply_hydrogen = Var(t, domain=NonNegativeReals)
-        block.feedin_hydrogen = Var(t, domain=NonNegativeReals)
+        block.hydrogen_balance = Var(t, domain=Reals)
+        block.hydrogen_supply = Var(t, domain=NonNegativeReals)
+        block.hydrogen_feedin = Var(t, domain=NonNegativeReals)
 
         block.hydrogen_in = Port()
         block.hydrogen_in.add(
-            block.feedin_hydrogen,
+            block.hydrogen_feedin,
             'hydrogen',
             Port.Extensive,
             include_splitfrac=False
             )
         block.hydrogen_out = Port()
         block.hydrogen_out.add(
-            block.supply_hydrogen,
+            block.hydrogen_supply,
             'hydrogen',
             Port.Extensive,
             include_splitfrac=False
             )
 
         # Declare construction rules for constraints
-        def max_supply_hydrogen_rule(_block, i):
+        def max_hydrogen_supply_rule(_block, i):
             """Rule for the maximal supply of hydrogen from the grid."""
-            return _block.supply_hydrogen[i] <= self.data.loc['max', 'hydrogen']
+            return _block.hydrogen_supply[i] <= self.data.loc['max', 'hydrogen']
         
-        def max_feedin_hydrogen_rule(_block, i):
+        def max_hydrogen_feedin_rule(_block, i):
             """Rule for the maximal feed in of hydrogen into the grid."""
-            return _block.feedin_hydrogen[i] <= self.data.loc['max', 'hydrogen']
+            return _block.hydrogen_feedin[i] <= self.data.loc['max', 'hydrogen']
         
-        def overall_hydrogen_rule(_block, i):
+        def hydrogen_balance_rule(_block, i):
             """Rule for calculating the overall hydrogen balance of the grid."""
-            return _block.overall_hydrogen[i] == _block.supply_hydrogen[i] - _block.feedin_hydrogen[i]
+            return _block.hydrogen_balance[i] == _block.hydrogen_supply[i] - _block.hydrogen_feedin[i]
           
         # Declare constraints
-        block.max_supply_hydrogen_constraint = Constraint(
+        block.max_hydrogen_supply_constraint = Constraint(
             t,
-            rule=max_supply_hydrogen_rule
+            rule=max_hydrogen_supply_rule
         )
-        block.max_feedin_hydrogen_constraint = Constraint(
+        block.max_hydrogen_feedin_constraint = Constraint(
             t,
-            rule=max_feedin_hydrogen_rule
+            rule=max_hydrogen_feedin_rule
         )
-        block.overall_hydrogen_constraint = Constraint(
+        block.hydrogen_balance_constraint = Constraint(
             t,
-            rule=overall_hydrogen_rule
+            rule=hydrogen_balance_rule
         )
 
 
@@ -126,11 +126,11 @@ class Grid:
         t = block.model().t
 
         # Declare components
-        block.overall_ngas = Var(t, domain=Reals)
+        block.ngas_balance = Var(t, domain=Reals)
 
         block.ngas_out = Port()
         block.ngas_out.add(
-            block.overall_ngas,
+            block.ngas_balance,
             'natural_gas',
             Port.Extensive,
             include_splitfrac=False
@@ -143,20 +143,20 @@ class Grid:
         t = block.model().t
 
         # Declare components
-        block.overall_heat = Var(t, domain=NonNegativeReals)
-        block.feedin_heat = Var(t, domain=NonNegativeReals)
-        block.supply_heat = Var(t, domain=NonNegativeReals)
+        block.heat_balance = Var(t, domain=NonNegativeReals)
+        block.heat_feedin = Var(t, domain=NonNegativeReals)
+        block.heat_supply = Var(t, domain=NonNegativeReals)
 
         block.heat_in = Port()
         block.heat_in.add(
-            block.feedin_heat,
+            block.heat_feedin,
             'heat',
             Port.Extensive,
             include_splitfrac=False
         )
         block.heat_out = Port()
         block.heat_out.add(
-            block.supply_heat,
+            block.heat_supply,
             'heat',
             Port.Extensive,
             include_splitfrac=False
@@ -164,23 +164,23 @@ class Grid:
 
         
         # Define construction rules for constraints
-        def overall_heat_rule(_block, i):
+        def heat_balance_rule(_block, i):
             """Rule for calculating the overall power."""
-            return _block.overall_heat[i] == (
+            return _block.heat_balance[i] == (
                 _block.model().heat_demand[i] 
-                + _block.supply_heat[i] 
-                - _block.feedin_heat[i]
+                + _block.heat_supply[i] 
+                - _block.heat_feedin[i]
                 )
         
         def supply_heat_demand_rule(_block, i):
             """Rule for fully suppling the heat demand."""
-            return _block.overall_heat[i] == 0
+            return _block.heat_balance[i] == 0
         
 
         # Define constraints
-        block.overall_power_constraint = Constraint(
+        block.heat_balance_constraint = Constraint(
             t,
-            rule=overall_heat_rule
+            rule=heat_balance_rule
         )
         block.supply_heat_demand_constraint = Constraint(
             t,
