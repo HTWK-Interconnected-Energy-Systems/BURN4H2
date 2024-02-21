4932a9924708c3a5648cbc70a917b242c9165042
mh <martin.hafemann@htwk-leipzig.de>
2024-02-21 17:33:10 +0100

    Added methods for data acquisition and model integration.

--- blocks/electrolyzer.py
+++ blocks/electrolyzer.py
@@ -1,11 +1,28 @@
 from pyomo.environ import *
 from pyomo.network import *
+import pandas as pd
 
 class Electrolyzer:
     """Class for constructing chp asset objects."""
 
-    def __init__(self, data) -> None:
-        self.data = data
+    def __init__(self, name, filepath, index_col=0) -> None:
+        self.name = name
+        self.get_data(filepath, index_col)
+    
+
+    def get_data(self, filepath, index_col):
+        """Collects data from a csv."""
+        self.data = pd.read_csv(
+            filepath,
+            index_col=index_col
+        )
+    
+
+    def add_to_model(self, model):
+        """Adds the asset as a pyomo block component to a given model."""
+        model.add_component(
+            self.name,
+            Block(rule=self.electrolyzer_block_rule))
 
     
     def electrolyzer_block_rule(self, block):
